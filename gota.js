bbea8b939340d15bd5c8a613fdf1ceb71e228f40
shehan.kalharaj@gmail.com <shehan.kalharaj@gmail.com>
2022-04-06 00:36:40 +0530

    first commit2

---
+++ gota.js
@@ -0,0 +1,14 @@
+let GoHomeGota=false;
+let noOfParliment=225;
+
+function saveNation(GoHomeGota,noOfParliment){
+    do{
+        keep();
+    }while(GoHomeGota===false && noOfParliment>0)
+
+}
+function keep(){
+    console.log("Go Home Gota!");
+}
+
+saveNation(GoHomeGota,noOfParliment);
